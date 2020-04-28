9af56036801624e6a2309a2db70aed5d9d6ffadd
zansol <hahn0406@gmail.com>
2020-04-28 23:52:09 +0900

    Add polyfill 추가

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(
   <React.StrictMode>
