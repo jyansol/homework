3ab756d7db84d5c3179dd6058826b9829751633a
zansol <hahn0406@gmail.com>
2020-04-28 23:47:40 +0900

    Modify 카드 컴포넌트 스타일링 수정

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,72 +6,98 @@ import Stars from '../Stars/index';
 const Card = ({ item }) => {
   const { label, title, highLight, crossOut, star, content, isRow } = item;
   return (
-    <Item row={isRow}>
-      <ImageContainer row={isRow}>
-        <img src={cardImg} alt="카드 이미지" />
-      </ImageContainer>
-      <ContentContainer>
-        {label && (
-          <Label>
-            {label}
-          </Label>
-        )}
-        {title && (
-          <Title>
-            {title}
-          </Title>
-        )}
-        {highLight && (
-          <HighLight>{highLight}</HighLight>
-        )}
-        {crossOut && (
-          <span>{crossOut}</span>
-        )}
-        {star && (
-          <StarContainer>
-            <Stars count={star} />
-          </StarContainer>
-        )}
-      </ContentContainer>
-    </Item>
+    <>
+      <Item row={isRow}>
+        <ImageContainer row={isRow}>
+          <img src={cardImg} alt="카드 이미지" />
+        </ImageContainer>
+        <Div>
+          <ContentContainer>
+            <Label>{label && label}</Label>
+            <Title>{title && title}</Title>
+            <HighLight>{highLight && highLight}</HighLight>
+            <CrossOut>{crossOut && crossOut}</CrossOut>
+          </ContentContainer>
+          {star && (
+            <StarContentContainer row={isRow}>
+              <StarContainer>
+                <Stars count={star} />&nbsp;| John Does
+              </StarContainer>
+            </StarContentContainer>
+          )}
+          {content && (
+            <Content>
+              {content}
+            </Content>
+          )}
+        </Div>
+      </Item>
+    </>
   )
 }
 const Item = styled.div`
   display: flex;
   flex-direction: ${props => (props.row ? 'row' : 'column')};
-  width: auto;
-  height: ${props => (props.row ? '160px' : 'auto')};
   border: 1px solid #D9D8D8;
   border-radius: 5px;
   background-color: #F8F9FB;
   overflow: hidden;
-  `;
+`;
 const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: ${props => (props.row ? '160px' : '200px')};
-  border: 1px solid red;
+  height: auto;
   img {
-    width: auto;
-    height: auto;
+    width: 100%;
+    height: 100%;
     max-width: 100%;
     max-height: 100%;
   }
 `;
+const Div = styled.div``;
 const ContentContainer = styled.div`
   width: auto;
   padding: 15px;
+`;
+const StarContentContainer = styled(ContentContainer)`
+  border-top: ${props => (props.row ? 'none' : '1px solid #bdbdbd')};
 `;
 const Label = styled.label`
   font-size: 12px;
   color: #bdbdbd;
 `;
 const Title = styled.h1`
-  margin: 0;
+  margin-top: 5px;
   font-size: 14px;
   font-weight: normal;
 `;
-const HighLight = styled.span``;
+const HighLight = styled.span`
+  display: inline-block;
+  color: tomato;
+  font-size: 14px;
+  margin-top: 25px;
+`;
+const CrossOut = styled.span`
+  text-decoration: line-through;
+  margin-left: 5px;
+  font-size: 12px;
+  color: #bdbdbd;
+`;
 const StarContainer = styled.div`
   display: flex;
   flex-direction: row;
+  color: #bdbdbd;
+  font-size: 12px;
+`;
+const Content = styled.div`
+  width: 160px;
+  font-size: 14px;
+  color: #323232;
+  padding: 0 15px 15px;
+  overflow:hidden; 
+  text-overflow:ellipsis;
+  white-space:nowrap;
 `;
 export default Card;
